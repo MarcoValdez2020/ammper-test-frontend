7ac9e2cc210c24212e551f8e04b1f93be9e08394
MARCO ANTONIO VALDEZ RODEA <valdez.rodea.marcoantonio@gmail.com>
2025-09-28 10:04:06 -0600

    Instalación de primeng y tailwind css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import ThemePreset from './app.theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: ThemePreset,
+        options: {
+          darkModeSelector: false || 'none',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
+        }
+      }
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, CardModule, InputTextModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ammper-pwa');
+  protected readonly message = signal('¡PrimeNG está funcionando correctamente!');
 }
